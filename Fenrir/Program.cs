44a43c53970114e494c69c6c2aaa6469dc34301c
David Gutierrez Rico <dagahh89@gmail.com>
2026-06-07 20:11:11 +0200

    Dockerfile

--- Fenrir/Program.cs
+++ Fenrir/Program.cs
@@ -2,10 +2,23 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
-
 var builder = WebApplication.CreateBuilder(args);
+
+// ---------------------------
+// SWAGGER
+// ---------------------------
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// ---------------------------
+// SERVICIOS
+// ---------------------------
 builder.Services.AddSingleton<ITokenService, TokenService>();
 builder.Services.AddSingleton<WarZeroService>();
+
+// ---------------------------
+// JWT
+// ---------------------------
 builder.Services
     .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
@@ -16,6 +29,7 @@ builder.Services
             ValidateAudience = true,
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
+
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
             ValidAudience = builder.Configuration["Jwt:Audience"],
             IssuerSigningKey = new SymmetricSecurityKey(
@@ -23,7 +37,26 @@ builder.Services
         };
     });
 
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+// ---------------------------
+// SWAGGER UI
+// ---------------------------
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/", () => Results.Ok("Fenrir API funcionando"));
+
 app.MapTokenEndpoints();
 app.MapWarZeroEndpoints();
+
 app.Run();
+
