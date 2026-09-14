1c24d8b1c7d7c989c49d4a7f44818df5c2dcbac1
David Gutierrez Rico <dagahh89@gmail.com>
2026-09-14 19:09:41 +0200

    .

--- Fenrir/Modules/WarZero/HistoriaCatalogo.cs
+++ Fenrir/Modules/WarZero/HistoriaCatalogo.cs
@@ -170,10 +170,10 @@ public static class HistoriaCatalogo
             Cuartel: null,
             Cartas: new[]
             {
-                new CartaHistoria(DemA, 6),
+                new CartaHistoria(DemA, 5),
                 new CartaHistoria(DemB, 1),
                 new CartaHistoria(DemC, 1),
-                new CartaHistoria(DemE, 1),
+                //new CartaHistoria(DemE, 1),
             },
             EnergiaInicial: 40),
         Bot: new BandoHistoria(
